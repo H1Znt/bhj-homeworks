bb37216918a4434cf102b53054369d6d2cf826f8
Andrey Maximov <arman.gen40@mail.ru>
2024-09-10 19:33:58 +0300

    Tabs, change .map on .forEach

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -2,12 +2,15 @@ const tabNavigation = Array.from(document.querySelectorAll('.tab'))
 const tabContent = Array.from(document.querySelectorAll('.tab__content'))
 
 document.querySelector('.tab__navigation').addEventListener('click', e => {
+  console.log(tabNavigation.indexOf(e.target))
+  console.log(e.target)
+  console.log(tabNavigation)
   if (e.target.classList.contains('tab')) {
     let tabSelect = tabNavigation.indexOf(e.target);
-    tabNavigation.map(item => item.classList.remove('tab_active'));
+    tabNavigation.forEach(item => item.classList.remove('tab_active'));
     tabNavigation[tabSelect].classList.add('tab_active')
 
-    tabContent.map(content => content.classList.remove('tab__content_active'));
+    tabContent.forEach(content => content.classList.remove('tab__content_active'));
     tabContent[tabSelect].classList.add('tab__content_active')
   }
 })
