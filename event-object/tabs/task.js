a6182b1a0f78a6ba0bad61ba1163cdebb9187dc3
Andrey Maximov <arman.gen40@mail.ru>
2024-09-10 19:44:42 +0300

    Tabs, remove concole.log

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -2,9 +2,6 @@ const tabNavigation = Array.from(document.querySelectorAll('.tab'))
 const tabContent = Array.from(document.querySelectorAll('.tab__content'))
 
 document.querySelector('.tab__navigation').addEventListener('click', e => {
-  console.log(tabNavigation.indexOf(e.target))
-  console.log(e.target)
-  console.log(tabNavigation)
   if (e.target.classList.contains('tab')) {
     let tabSelect = tabNavigation.indexOf(e.target);
     tabNavigation.forEach(item => item.classList.remove('tab_active'));
